6f1b743bfa3e991885313c444fcfaf30e6807963
adoletskiy <adoletsky@indigos.ru>
2015-09-30 17:01:20 +0300

    debug mainMenu

---
+++ bitrix/templates/.default/components/bitrix/catalog.section.list/mainCatalogMenuV20/template.php
@@ -0,0 +1,171 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item">iPhone 5S</a>
+
+                    <!-- submenu -->
+                    <ul class="submenu-catalog js-submenu-catalog">
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5S 16gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5S 32gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5S 64gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5S VIP</a>
+
+                            <!-- submenu three -->
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <ul class="submenu-three-catalog js-submenu-three-catalog">
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Diamond</a>
+                                </li>
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Gold</a>
+                                </li>
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Светояблоко</a>
+                                </li>
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Цветной</a>
+                                </li>
+                            </ul>
+                            <!-- /submenu three -->
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">Аксессуары для iPhone 5S</a>
+
+                            <!-- submenu three -->
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <ul class="submenu-three-catalog js-submenu-three-catalog">
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">Защитные пленки для iPhone 5S</a>
+                                </li>
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">Аксессуары для автомобиля для iPhone 5S</a>
+                                </li>
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">Внешние аккумуляторы для iPhone 5S</a>
+                                </li>
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">Зарядные устройства и док-станции для iPhone 5S</a>
+                                </li>
+                            </ul>
+                            <!-- /submenu three -->
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">Чехлы для iPhone 5S</a>
+                        </li>
+                    </ul>
+                    <!-- /submenu -->
+                </li>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item">iPhone 5C</a>
+
+                    <!-- submenu -->
+                    <ul class="submenu-catalog js-submenu-catalog">
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5C 16gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5C 32gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">Аксессуары для iPhone 5C</a>
+
+                            <!-- submenu three -->
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <ul class="submenu-three-catalog js-submenu-three-catalog">
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Diamond</a>
+                                </li>
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Gold</a>
+                                </li>
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Светояблоко</a>
+                                </li>
+                                <li class="submenu-three-catalog-item">
+                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Цветной</a>
+                                </li>
+                            </ul>
+                            <!-- /submenu three -->
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">Чехлы для iPhone 5C</a>
+                        </li>
+                    </ul>
+                    <!-- /submenu -->
+                </li>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item">
+                        iPhone 5
+                    </a>
+
+                    <!-- submenu -->
+                    <ul class="submenu-catalog js-submenu-catalog">
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5 16gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5 32gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5 64gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5 VIP</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPhone 5 цветной</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">Бамперы для iPhone 5</a>
+                        </li>
+                    </ul>
+                    <!-- /submenu -->
+                </li>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item">iPad Air</a>
+
+                    <!-- submenu -->
+                    <ul class="submenu-catalog js-submenu-catalog">
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPad Air 5 16 Gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPad Air 5 32 Gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPad Air 5 64 Gb</a>
+                        </li>
+                        <li class="submenu-catalog-item">
+                            <a href="#" class="submenu-catalog-link">iPad Air 5 128 Gb</a>
+                        </li>
+                    </ul>
+                    <!-- /submenu -->
+                </li>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item">iPad Mini 2</a>
+                </li>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item">iPhone 4/4s</a>
+                </li>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item">MacBook</a>
+                </li>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item">iMac</a>
+                </li>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item">Гаджеты</a>
+                </li>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item">Бренды</a>
+                </li>
+                <li class="menu-catalog-list-item">
+                    <a href="#" class="menu-catalog-item current">Скидки</a>
+                </li>
+
--- bitrix/templates/index_v20/header.php
+++ bitrix/templates/index_v20/header.php
@@ -89,11 +89,6 @@ IncludeTemplateLangFile(__FILE__); ?>
                         ),
                         false
                     );?>
-<!--                    <a href="#" class="menu-page-item">Новости</a>-->
-<!--                    <a href="#" class="menu-page-item">Оплата</a>-->
-<!--                    <a href="#" class="menu-page-item">Доставка</a>-->
-<!--                    <a href="#" class="menu-page-item">Кредит</a>-->
-<!--                    <a href="#" class="menu-page-item">Гарантия</a>-->
                 </nav>
 
                 <div class="clearfix">
@@ -127,175 +122,29 @@ IncludeTemplateLangFile(__FILE__); ?>
     <div class="col-xs-12">
         <nav class="menu-catalog-block clearfix">
             <ul class="menu-catalog-list">
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item">iPhone 5S</a>
+                <?$APPLICATION->IncludeComponent("bitrix:catalog.section.list", "mainCatalogMenuV20", array(
+                        "IBLOCK_TYPE" => "1c_catalog",
+                        "IBLOCK_ID" => "8",
+                        "SECTION_ID" => "",
+                        "SECTION_CODE" => "",
+                        "COUNT_ELEMENTS" => "N",
+                        "TOP_DEPTH" => "3",
+                        "SECTION_FIELDS" => array(
+                            0 => "",
+                            1 => "",
+                        ),
+                        "SECTION_USER_FIELDS" => array(
+                            "UF_ACTIVE",
+                        ),
+                        "SECTION_URL" => "/#CODE#",
+                        "CACHE_TYPE" => "A",
+                        "CACHE_TIME" => "36000000",
+                        "CACHE_GROUPS" => "Y",
+                        "ADD_SECTIONS_CHAIN" => "Y",
+                    ),
+                    false
+                );?>
 
-                    <!-- submenu -->
-                    <ul class="submenu-catalog js-submenu-catalog">
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5S 16gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5S 32gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5S 64gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5S VIP</a>
-
-                            <!-- submenu three -->
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                            <ul class="submenu-three-catalog js-submenu-three-catalog">
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Diamond</a>
-                                </li>
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Gold</a>
-                                </li>
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Светояблоко</a>
-                                </li>
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Цветной</a>
-                                </li>
-                            </ul>
-                            <!-- /submenu three -->
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">Аксессуары для iPhone 5S</a>
-
-                            <!-- submenu three -->
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                            <ul class="submenu-three-catalog js-submenu-three-catalog">
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">Защитные пленки для iPhone 5S</a>
-                                </li>
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">Аксессуары для автомобиля для iPhone 5S</a>
-                                </li>
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">Внешние аккумуляторы для iPhone 5S</a>
-                                </li>
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">Зарядные устройства и док-станции для iPhone 5S</a>
-                                </li>
-                            </ul>
-                            <!-- /submenu three -->
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">Чехлы для iPhone 5S</a>
-                        </li>
-                    </ul>
-                    <!-- /submenu -->
-                </li>
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item">iPhone 5C</a>
-
-                    <!-- submenu -->
-                    <ul class="submenu-catalog js-submenu-catalog">
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5C 16gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5C 32gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">Аксессуары для iPhone 5C</a>
-
-                            <!-- submenu three -->
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                            <ul class="submenu-three-catalog js-submenu-three-catalog">
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Diamond</a>
-                                </li>
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Gold</a>
-                                </li>
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Светояблоко</a>
-                                </li>
-                                <li class="submenu-three-catalog-item">
-                                    <a href="#" class="submenu-three-catalog-link">iPhone 5S Цветной</a>
-                                </li>
-                            </ul>
-                            <!-- /submenu three -->
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">Чехлы для iPhone 5C</a>
-                        </li>
-                    </ul>
-                    <!-- /submenu -->
-                </li>
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item">
-                        iPhone 5
-                    </a>
-
-                    <!-- submenu -->
-                    <ul class="submenu-catalog js-submenu-catalog">
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5 16gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5 32gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5 64gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5 VIP</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPhone 5 цветной</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">Бамперы для iPhone 5</a>
-                        </li>
-                    </ul>
-                    <!-- /submenu -->
-                </li>
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item">iPad Air</a>
-
-                    <!-- submenu -->
-                    <ul class="submenu-catalog js-submenu-catalog">
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPad Air 5 16 Gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPad Air 5 32 Gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPad Air 5 64 Gb</a>
-                        </li>
-                        <li class="submenu-catalog-item">
-                            <a href="#" class="submenu-catalog-link">iPad Air 5 128 Gb</a>
-                        </li>
-                    </ul>
-                    <!-- /submenu -->
-                </li>
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item">iPad Mini 2</a>
-                </li>
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item">iPhone 4/4s</a>
-                </li>
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item">MacBook</a>
-                </li>
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item">iMac</a>
-                </li>
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item">Гаджеты</a>
-                </li>
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item">Бренды</a>
-                </li>
-                <li class="menu-catalog-list-item">
-                    <a href="#" class="menu-catalog-item current">Скидки</a>
-                </li>
             </ul>
         </nav>
     </div>
