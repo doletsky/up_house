3ced75366850b9f5d227ce1f4181d742e2ee542d
adoletskiy <adoletsky@indigos.ru>
2015-09-30 13:41:18 +0300

    topMenu

--- bitrix/templates/index_v20/header.php
+++ bitrix/templates/index_v20/header.php
@@ -73,11 +73,27 @@ IncludeTemplateLangFile(__FILE__); ?>
                         <br>
                         <div class="call_back_button">Перезвоните мне</div>
                     </div>
-                    <a href="#" class="menu-page-item">Новости</a>
-                    <a href="#" class="menu-page-item">Оплата</a>
-                    <a href="#" class="menu-page-item">Доставка</a>
-                    <a href="#" class="menu-page-item">Кредит</a>
-                    <a href="#" class="menu-page-item">Гарантия</a>
+                    <?$APPLICATION->IncludeComponent("bitrix:menu", "topMenu", array(
+                            "ROOT_MENU_TYPE" => "top",
+                            "MENU_CACHE_TYPE" => "N",
+                            "MENU_CACHE_TIME" => "3600",
+                            "MENU_CACHE_USE_GROUPS" => "Y",
+                            "MENU_CACHE_GET_VARS" => array(
+                            ),
+                            "MAX_LEVEL" => "1",
+                            "CHILD_MENU_TYPE" => "left",
+                            "USE_EXT" => "N",
+                            "DELAY" => "N",
+                            "ALLOW_MULTI_SELECT" => "N",
+
+                        ),
+                        false
+                    );?>
+<!--                    <a href="#" class="menu-page-item">Новости</a>-->
+<!--                    <a href="#" class="menu-page-item">Оплата</a>-->
+<!--                    <a href="#" class="menu-page-item">Доставка</a>-->
+<!--                    <a href="#" class="menu-page-item">Кредит</a>-->
+<!--                    <a href="#" class="menu-page-item">Гарантия</a>-->
                 </nav>
 
                 <div class="clearfix">
