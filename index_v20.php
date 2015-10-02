b5a55ad885d37881ca4010f81b8e5187a8c48d4e
adoletskiy <adoletsky@indigos.ru>
2015-10-02 10:27:29 +0300

    debug news block

--- index_v20.php
+++ index_v20.php
@@ -8,50 +8,98 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
     <div class="col-xs-12">
 
     <!-- новости и обзоры -->
-    <section class="news-reviews main-section">
-        <div class="row">
-            <div class="col-xs-9">
-                <div>
-                    <h2 class="news-reviews-title entry-title">Новости и обзоры</h2>
-                    <div class="news-reviews-content">
-                        <article class="news-reviews-item">
-                            <time class="news-reviews-date" datetime="16.06.2014">16.06.2014</time>
-                            <a href="#" class="news-reviews-text">Metal - будущее мобильной графики</a>
-                        </article>
-
-                        <article class="news-reviews-item">
-                            <time class="news-reviews-date" datetime="15.06.2014">15.06.2014</time>
-                            <a href="#" class="news-reviews-text">OS X Yosemite и iOS 8: на какие устройства можно установить новые ОС?</a>
-                        </article>
-
-                        <article class="news-reviews-item">
-                            <time class="news-reviews-date" datetime="11.06.2014">11.06.2014</time>
-                            <a href="#" class="news-reviews-text">График работы нашего магазина на июньские праздники</a>
-                        </article>
-
-                        <article class="news-reviews-item">
-                            <time class="news-reviews-date" datetime="09.06.2014">09.06.2014</time>
-                            <a href="#" class="news-reviews-text">HomeKit - «умный дом» под полным контролем</a>
-                        </article>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-xs-3">
-
-                <!-- подписаться -->
-                <aside class="subscribe">
-                    <div class="subscribe-content">
-                        <div class="subscribe-title">Узнавайте первым<br /> о наших акциях</div>
-
-                        <input type="text" class="subscribe-input" placeholder="имя" />
-                        <input type="email" class="subscribe-input" placeholder="email" />
-                        <input type="submit" class="subscribe-button" value="Подписаться" />
-                    </div>
-                </aside>
-            </div>
-        </div>
-    </section>
+    <?$APPLICATION->IncludeComponent("bitrix:news.list", "lastNews", array(
+            "IBLOCK_TYPE" => "news",
+            "IBLOCK_ID" => "1",
+            "NEWS_COUNT" => "4",
+            "SORT_BY1" => "ACTIVE_FROM",
+            "SORT_ORDER1" => "DESC",
+            "SORT_BY2" => "SORT",
+            "SORT_ORDER2" => "ASC",
+            "FILTER_NAME" => "",
+            "FIELD_CODE" => array(
+                0 => "",
+                1 => "",
+            ),
+            "PROPERTY_CODE" => array(
+                0 => "",
+                1 => "",
+            ),
+            "CHECK_DATES" => "Y",
+            "DETAIL_URL" => "show_news_#CODE#.html",
+            "AJAX_MODE" => "N",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "Y",
+            "AJAX_OPTION_HISTORY" => "N",
+            "CACHE_TYPE" => "A",
+            "CACHE_TIME" => "36000000",
+            "CACHE_FILTER" => "N",
+            "CACHE_GROUPS" => "Y",
+            "PREVIEW_TRUNCATE_LEN" => "",
+            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+            "SET_TITLE" => "N",
+            "SET_STATUS_404" => "N",
+            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+            "ADD_SECTIONS_CHAIN" => "N",
+            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+            "PARENT_SECTION" => "",
+            "PARENT_SECTION_CODE" => "",
+            "DISPLAY_TOP_PAGER" => "N",
+            "DISPLAY_BOTTOM_PAGER" => "N",
+            "PAGER_TITLE" => "Новости",
+            "PAGER_SHOW_ALWAYS" => "N",
+            "PAGER_TEMPLATE" => "",
+            "PAGER_DESC_NUMBERING" => "N",
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+            "PAGER_SHOW_ALL" => "N",
+            "AJAX_OPTION_ADDITIONAL" => ""
+        ),
+        false
+    );?>
+<!--    <section class="news-reviews main-section">-->
+<!--        <div class="row">-->
+<!--            <div class="col-xs-9">-->
+<!--                <div>-->
+<!--                    <h2 class="news-reviews-title entry-title">Новости и обзоры</h2>-->
+<!--                    <div class="news-reviews-content">-->
+<!--                        <article class="news-reviews-item">-->
+<!--                            <time class="news-reviews-date" datetime="16.06.2014">16.06.2014</time>-->
+<!--                            <a href="#" class="news-reviews-text">Metal - будущее мобильной графики</a>-->
+<!--                        </article>-->
+<!---->
+<!--                        <article class="news-reviews-item">-->
+<!--                            <time class="news-reviews-date" datetime="15.06.2014">15.06.2014</time>-->
+<!--                            <a href="#" class="news-reviews-text">OS X Yosemite и iOS 8: на какие устройства можно установить новые ОС?</a>-->
+<!--                        </article>-->
+<!---->
+<!--                        <article class="news-reviews-item">-->
+<!--                            <time class="news-reviews-date" datetime="11.06.2014">11.06.2014</time>-->
+<!--                            <a href="#" class="news-reviews-text">График работы нашего магазина на июньские праздники</a>-->
+<!--                        </article>-->
+<!---->
+<!--                        <article class="news-reviews-item">-->
+<!--                            <time class="news-reviews-date" datetime="09.06.2014">09.06.2014</time>-->
+<!--                            <a href="#" class="news-reviews-text">HomeKit - «умный дом» под полным контролем</a>-->
+<!--                        </article>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="col-xs-3">-->
+<!---->
+<!--                <!-- подписаться -->-->
+<!--                <aside class="subscribe">-->
+<!--                    <div class="subscribe-content">-->
+<!--                        <div class="subscribe-title">Узнавайте первым<br /> о наших акциях</div>-->
+<!---->
+<!--                        <input type="text" class="subscribe-input" placeholder="имя" />-->
+<!--                        <input type="email" class="subscribe-input" placeholder="email" />-->
+<!--                        <input type="submit" class="subscribe-button" value="Подписаться" />-->
+<!--                    </div>-->
+<!--                </aside>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </section>-->
     <!-- /новости и обзоры -->
 
     <!-- новинки -->
